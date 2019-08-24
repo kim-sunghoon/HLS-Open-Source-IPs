b0b97e4bf4e194c9816d43e0ce6695b434394ae8
kim-sunghoon <sunghoon1kim@postech.ac.kr>
2019-08-24 13:22:03 +0900

    dwconv3x3 ip update - partition pragma DEPTH did not work... how can we modify it? automatic search like keras tuner...?

--- DW-CONV-IP/dwconv3x3.cc
+++ DW-CONV-IP/dwconv3x3.cc
@@ -2,29 +2,24 @@
 
 // conv 3x3 for group (depth-wise convolutions)
 
-#include <stdio.h>
-#include <math.h>
-#include <ap_fixed.h>
-#include "hls_stream.h"
 #include "dcl.h"
 
-
 void load_weights(FIX_WT weight_buf[DEPTH],
-				  FIX_WT weights[DEPTH][3][3],
+				  FIX_WT weights[DEPTH][K][K],
 				  int i, int j)
 {
 #pragma HLS ARRAY_PARTITION variable=weights dim=1 factor=16
 
-	for(int coo = 0; coo < 16; coo++){
+	for(int coo = 0; coo < DEPTH; coo++){
 #pragma HLS unroll
 		weight_buf[coo] = weights[coo][i][j];
 	}
 }
 
 
-void CONV_3x3_group(FIX_FM bottom[DEPTH][HEIGH][WIDTH],
+void dwconv3x3(FIX_FM bottom[DEPTH][HEIGH][WIDTH],
 					FIX_FM top[DEPTH][HEIGH][WIDTH],
-					FIX_WT weights[DEPTH][3][3])
+					FIX_WT weights[DEPTH][K][K])
 {
 
 	FIX_WT weight_buf[DEPTH];
@@ -34,8 +29,8 @@ void CONV_3x3_group(FIX_FM bottom[DEPTH][HEIGH][WIDTH],
 #pragma HLS ARRAY_PARTITION variable=weight_buf complete
 
 
-	for(int i = 0; i < 3; i++){
-		for(int j = 0; j < 3; j++){
+	for(int i = 0; i < K; i++){
+		for(int j = 0; j < K; j++){
 
 #pragma HLS dataflow
 
@@ -44,7 +39,7 @@ void CONV_3x3_group(FIX_FM bottom[DEPTH][HEIGH][WIDTH],
 			for(int h = 1; h <= HEIGH-2; h++){
 				for(int w = 1; w <= WIDTH-2; w++){
 #pragma HLS pipeline
-					for(int co = 0; co < 16; co++){
+					for(int co = 0; co < DEPTH; co++){
 #pragma HLS unroll
 						top[co][h][w] += weight_buf[co] * bottom[co][h+i-1][w+j-1];
 					}
